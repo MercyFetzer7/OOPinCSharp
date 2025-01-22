762e7aecc1208a315d8c542d967bf64d819ab783
Mercy Fetzer <131705906+MercyFetzer7@users.noreply.github.com>
2025-01-21 21:04:07 -0700

    Update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// User enters a quote
+// Return:
+// How many words
+// How many characters
+// How many of each letter was used
+
+
+using OOPinCSharp;
+
+string quote = ""; // set it equal to something so it's not a NULL value
+int numWords = 0;
+
+// User input
+Console.WriteLine("Please enter a quote to analyze: ");
+quote = Console.ReadLine(); // set user input to the quote variable
+
+Console.WriteLine("Quote: " + quote);
+
+WordTools wt = new WordTools(); // Declaration and Instantiation
+numWords = wt.WordCounter(quote);
+
+
+Console.WriteLine("Number of Words: " + numWords);
+
+Console.WriteLine("Number of Characters: " + wt.CharacterCounter(quote));
+
+Console.WriteLine("Alphabet: ");
+
+wt.AlphabetCounter(quote);
--- WordTools.cs
+++ WordTools.cs
@@ -1,6 +1,48 @@
-namespace OOPinCSharp;
-
-public class WordTools
+namespace OOPinCSharp
 {
-    
+    internal class WordTools
+    {
+        public int WordCounter(string words)
+        {
+            int wordCount = 0;
+            
+            // this is splitting the string into separate words to make it easier to count
+            string [] individualWords = words.Split(' '); // this is a function that splits up a string based in what you want
+            
+            wordCount = individualWords.Length;
+            return (wordCount);
+        }
+
+        public int CharacterCounter(string words)
+        {
+            int characterCount = 0;
+            characterCount = words.Length;
+            return (characterCount);
+        }
+
+        public void AlphabetCounter(string words)
+        {
+            int[] letters = new int[26]; // for the alphabet, beginning with 'a' and going to 'z'
+
+            // Loop through the entire string
+            for (int i = 0; i < words.Length; i++) // i++ increments i by 1
+            {
+                char c = words[i];
+                
+                c = Char.ToLower(c); // char is a library
+                
+                // Check to make sure it is a lowercase letter
+                if (c >= 97 && c <= 122)
+                { 
+                    letters[(c - 97)]++;
+                }
+            }
+            // Print results from the array
+            for (int i = 0; i < letters.Length; i++)
+            {
+                char letter = (char)(i + 97);
+                Console.WriteLine(letter + ": " + letters[i]);
+            }
+        }
+    }
 }
